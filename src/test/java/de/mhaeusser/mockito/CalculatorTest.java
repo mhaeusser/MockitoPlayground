a2aeb5460e960a68f405656216ff3b0f1bc2271a
Matthias Häußer <Matthias.Haeusser@EXXETA.com>
2019-10-09 23:34:11 +0200

    Use Mockito.

--- src/test/java/de/mhaeusser/mockito/CalculatorTest.java
+++ src/test/java/de/mhaeusser/mockito/CalculatorTest.java
@@ -2,20 +2,26 @@ package de.mhaeusser.mockito;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 public class CalculatorTest {
 
     Calculator calc = null;
+    CalculatorService service = Mockito.mock(CalculatorService.class);
 
     @Before
     public void setup() {
-        calc = new Calculator((i, j) -> i+j);
+        calc = new Calculator(service);
     }
 
     @Test
     public void testAdd() {
+        when(service.add(2, 3)).thenReturn(5);
         assertEquals(5, calc.add(2, 3));
+        verify(service).add(2, 3);
     }
 }
